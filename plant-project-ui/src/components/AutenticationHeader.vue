0faf76e0f4279bc0d6bb38793720e8f85dbfa56f
DamlaSub <mme.dalloway@gmail.com>
2023-03-27 23:20:21 +0200

    add ui in vue js

---
+++ plant-project-ui/src/components/AutenticationHeader.vue
@@ -0,0 +1,60 @@
+<template>
+<div className='authentification-header'>
+<nav>
+<ul  className="auth-navbar">
+    <li><img src="myLogo" alt="SOS plant project" className="Navlink logo"/></li>
+    <li><a href="/" className="authentification-Navlink Sos">SOS PLANTES</a></li>
+    <li className='Navlink-signup'><a href='/signup'> <button className='Navlink-signupbtn'>Créer un compte</button></a></li>
+    <li className='Navlink-login'><a  href='/login'><button className="Navlink-loginbtn">Se connecter</button></a></li>
+  
+         
+</ul>
+</nav>    
+</div>
+</template>
+
+<style scoped>
+.authentification-header {
+    margin: 0%;
+  position: relative;
+  z-index: 10;
+  height: 96px;
+  border-bottom: 1px solid #000;
+  background-color: #f9f5f1;
+}
+.authentification-header .logo{
+    height: 3vw;
+ 
+}
+.auth-navbar {
+    list-style-type: none;
+    margin: 0%;
+
+  }
+
+.authentification-Navlink{
+
+    display: inline-block;
+    color: black;
+    text-align: center;
+    padding: 30px 10px;
+    text-decoration: none;
+    float: left;
+}
+.auth-navbar .Sos {
+font-size: 30px;
+font-weight: 900;
+color: #025050;
+padding-left: 35%;
+}
+
+.auth-navbar .Navlink:hover {
+  text-decoration: underline;
+  text-decoration-color:   #d5f5e3 ;
+  text-decoration-thickness: 9px;
+}
+
+.auth-navbar .Sos:hover{
+text-decoration: none;
+}
+</style>
---
+++ plant-project-ui/src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from "vue-router";
+import LoginView from "../views/LoginView.vue";
+import SignupView from "../views/SignupView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      //component:
+    },
+    {
+      path: "/login",
+      name: "login",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/LoginView.vue"),
+    },
+    {
+      path: "/signup",
+      name: "signup",
+      component: () => import("../views/SignupView.vue"),
+    },
+  ],
+});
+
+export default router;
