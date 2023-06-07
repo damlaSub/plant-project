4194bbefbd14e0a6b42a6ec1812709f7f2bb81ee
DamlaSub <mme.dalloway@gmail.com>
2023-06-07 12:05:54 +0200

    add WebConfig

--- plant-project-api/src/main/java/co/simplon/plantproject/PlantProjectApiApplication.java
+++ plant-project-api/src/main/java/co/simplon/plantproject/PlantProjectApiApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PlantProjectApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PlantProjectApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(
+		PlantProjectApiApplication.class, args);
+	System.out.println("Hi there! :)");
+    }
 
 }
