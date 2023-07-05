8b453b47fcc70e84ad61062583f0bff0e408df9a
DamlaSub <mme.dalloway@gmail.com>
2023-07-05 23:29:44 +0200

    filter plants by name

--- plant-project-ui/src/components/Home.vue
+++ plant-project-ui/src/components/Home.vue
@@ -4,6 +4,7 @@
       return {
         baseUrl: import.meta.env.VITE_IMG_BASE_URL,
         plants: [],
+        input: "",
       };
     },
     methods: {
@@ -11,16 +12,94 @@
         const resp = await this.$http.get("/plants");
         this.plants = resp.body;
       },
+      filteredPlantList() {
+        return this.plants.filter((plant) =>
+          plant.name.toLowerCase().includes(this.input.toLowerCase())
+        );
+      },
     },
     beforeMount() {
       this.initPlants();
     },
+    mounted() {
+      this.filteredPlantList();
+    },
   };
 </script>
 <template>
-  <h1>All plants</h1>
+  <div class="input-group mb-3 p-5">
+    <span class="input-group-text" id="basic-addon1"
+      ><i class="bi bi-search"></i
+    ></span>
+
+    <input
+      type="text"
+      v-model.trim="input"
+      ref="input"
+      class="form-control"
+      placeholder="Search plants..."
+      aria-label="Search plants"
+      aria-describedby="basic-addon1"
+    />
+  </div>
+  <div class="row">
+    <div class="col" role="group">
+      <button
+        type="button"
+        class="btn btn-outline-secondary"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        Sun <i class="bi bi-chevron-down"></i>
+      </button>
+      <ul class="dropdown-menu">
+        <li><img src="../../public/images/sun.svg" /></li>
+        <li>
+          <img src="../../public/images/sun.svg" /><img
+            src="../../public/images/sun.svg"
+          />
+        </li>
+        <li>
+          <img src="../../public/images/sun.svg" /><img
+            src="../../public/images/sun.svg"
+          /><img src="../../public/images/sun.svg" />
+        </li>
+      </ul>
+    </div>
+    <div class="col" role="group">
+      <button
+        type="button"
+        class="btn btn-outline-secondary"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        Water <i class="bi bi-chevron-down"></i>
+      </button>
+      <ul class="dropdown-menu">
+        <li>
+          <img src="../../public/images/water.svg" />
+        </li>
+        <li>
+          <img src="../../public/images/water.svg" /><img
+            src="../../public/images/water.svg"
+          />
+        </li>
+        <li>
+          <img src="../../public/images/water.svg" /><img
+            src="../../public/images/water.svg"
+          /><img src="../../public/images/water.svg" />
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div
+    class="p-5 item error text-danger"
+    v-if="input && !filteredPlantList().length"
+  >
+    <p>No results found!</p>
+  </div>
   <div class="row row-cols-md-4 g-3 p-5">
-    <div class="col" v-for="plant in plants">
+    <div class="col" v-for="plant in filteredPlantList()" :key="plant">
       <div class="card h-100">
         <div class="p-3">
           <img
@@ -53,3 +132,8 @@
     </div>
   </div>
 </template>
+<style>
+  button {
+    background-color: #f4ede7;
+  }
+</style>
--- plant-project-ui/src/views/HomeView.vue
+++ plant-project-ui/src/views/HomeView.vue
@@ -1,8 +1,6 @@
 <script setup>
   import Home from "../components/Home.vue";
-  import SearchBar from "../components/SearchBar.vue";
 </script>
 <template>
-  <SearchBar />
   <Home />
 </template>
